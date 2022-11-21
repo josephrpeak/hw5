1af6612cd6a31cebf961534190c157429b378259
Joseph Peak <55969233+josephrpeak@users.noreply.github.com>
2022-11-21 01:49:32 -0600

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -23,7 +23,44 @@ class House():
         self.current_value = self.currentValue()
         return f"--------------------\nHouse Information:\n Year Built: {self.year}\n Purchase Price: {self.principal}\n Current Value of House: {round(self.current_value, 2)}\n Location: {self.location}\n--------------------"
         
-            
+
+def getValid(tp, msg, emsg, lb=-1, ub=-1, lst=None):
+    args = len(locals())
+    
+    while(True):
+        if(lb != -1 and ub != -1):
+            try:
+                var = tp(input(msg))
+                if(var < lb or var > ub):
+                    raise ValueError
+                else:
+                    break
+            except ValueError:
+                print(emsg)
+                
+        elif(lb != -1):
+            try:
+                var = tp(input(msg))
+                if(var < lb):
+                    raise ValueError
+                else:
+                    break
+            except ValueError:
+                print(emsg)
+
+        elif(lst != None):
+            try:
+                var = tp(input(msg))
+                if(var.upper() not in lst):
+                    raise ValueError
+                else:
+                    break
+
+            except ValueError:
+                print(emsg)
+                   
+    return var
+                
 def main():
     states = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
 
@@ -31,51 +68,14 @@ def main():
     
     while(retry == True):
         print("Welcome to our house calculation app.")
-
-        while(True):
-            try:
-                model_year = int(input("Enter house model year: "))
-                if(model_year < 0 or model_year > 2022):
-                    raise ValueError
-                else:
-                    break
-                
-            except ValueError:
-                print("Please enter a valid year.")
-
         
-        while(True):
-            try:
-                house_price = int(input("Price of the house: "))
-                if(house_price < 1):
-                    raise ValueError
-                else:
-                    break
-                
-            except ValueError:
-                print("Please enter a valid price.")
+        model_year = getValid(int, "Enter house model year: ", "Please enter a valid year.", 0, 2022)
 
-        while(True):
-            try:
-                current_year = int(input("Current year: "))
-                if(current_year < model_year or current_year > 2022):
-                    raise ValueError
-                else:
-                    break
-                
-            except ValueError:
-                print("Please enter the correct current year")
+        house_price = getValid(int, "Price of the house: ", "Please enter a valid price.", 1)
 
-        while(True):
-            try:
-                house_location = input("Enter your house location: ")
-                if(house_location.upper() not in states):
-                    raise ValueError
-                else:
-                    break
-                
-            except ValueError:
-                print("Please enter a valid location.")
+        current_year = getValid(int, "Current year: ", "Please enter the correct current year.", model_year, 2022)
+
+        house_location = getValid(str, "Enter your house location: ", "Please enter a valid location.", -1, -1, states)
         
         house = House(model_year, house_price, house_location.upper(), 0, current_year - model_year)
         house.current_value = house.currentValue()
